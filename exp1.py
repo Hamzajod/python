389a5a723006f362219743e3fba2816510145094
Hamzajod <hamzajod@eng.rizvi.edu.in>
2025-02-12 11:38:57 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,5 @@
+principle=float(input("enter principle amount: "))
+rate=float(input("Enter the rate of interest(% per year):"))
+time=float(input("enter the time in years: "))
+simpleinterest=(principle*rate*time)/100
+print(simpleinterest)
