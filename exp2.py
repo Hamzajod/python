0ff6335bfcbfee1c62a8352e13b4e16c90cc070e
Hamzajod <hamzajod@eng.rizvi.edu.in>
2025-02-12 12:05:38 +0530

    Create exp2.py

---
+++ exp2.py
@@ -0,0 +1,17 @@
+num1=float(input("enter the first number: "))
+num2=float(input("enter thje second number: "))
+
+addition = num1+num2
+substraction = num1-num2
+multiplication = num1*num2
+division = num1/num2
+modulus = num1%num2
+expo = num1**num2
+
+print("addition=",addition)
+print("substraction=",substraction)
+print("multiplication=",multiplication)
+print("division=",division)
+print("modulus=",modulus)
+print("expo=",expo)
+         
